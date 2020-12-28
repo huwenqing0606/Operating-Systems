cde0bf43084cd1fe2d2cfc19426dd3a1ffe72052
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-12-28 08:17:41 -0600

    Add files via upload

--- Labs/Lab1_Util/sleep.c
+++ Labs/Lab1_Util/sleep.c
@@ -1,1 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
 
+#define STDIN_FILENO  0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
+int
+main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+	fprintf(STDERR_FILENO, "Wenqing says: Syntax error! Use: sleep <number>\n");
+        exit(1);
+    }
+    int number = atoi(argv[1]);
+    sleep(number);
+    exit(0);
+}
