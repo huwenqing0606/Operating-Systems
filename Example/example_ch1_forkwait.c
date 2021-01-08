3009d99002e2299c6de527152dd8f7b076b00773
Dr_HuWenqing <huwenqing.pku@gmail.com>
2021-01-07 18:20:41 -0600

    Add files via upload

--- Example/example_ch1_forkwait.c
+++ Example/example_ch1_forkwait.c
@@ -1,14 +1,13 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-void 
+int 
 main(){
 	int pid = fork();
 	if(pid>0){
-		printf("parent: child=%d\n", pid);
+		//printf("parent: child=%d\n", pid);
 		pid = wait((int *) 0);
-		printf("parent: child %d is done\n", pid); //the two processes: parent and child, may be excuted simutaneously
-		//output may look like: parcehnitl: dc:h ielxdi=ti4ng
+		printf("parent: child %d is done\n", pid);
 		exit(0);
 	} else if(pid==0){
 		printf("child: exiting\n");
--- Example/example_lec1_copy.c
+++ Example/example_lec1_copy.c
@@ -15,7 +15,5 @@ main()
     write(1, buf, n);
   }
 
-  printf("Wenqing says: finishing!\n");
-
   exit(0);
 }
