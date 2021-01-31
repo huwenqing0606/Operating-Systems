d472126a8c0df28c40751bb134658b50cf4f81b1
Dr_HuWenqing <huwenqing.pku@gmail.com>
2021-01-30 21:56:07 -0600

    Add files via upload

--- xargs.c
+++ xargs.c
@@ -14,14 +14,16 @@ int main(int argc, char *argv[])
     int n, args_index = 0;
     int i;
 
-    // in each line, argv[1] is the command cmd
-    // argv[2],...,argv[argc-1] are the paramters
+    // argv[1] is the command cmd to be executed
+    // argv[1],...,argv[argc-1] are the parameters in the original command
+    // the first parameter is still argv[1], since for exec it is the name of the command
     char* cmd = argv[1];
     for (i = 1; i < argc; i++) params[args_index++] = argv[i];
 
     while ((n = read(IN, line, MAXLINE)) > 0) 
     // n is the number of bytes read from input and saved to line
     // read until the end of the IN file
+    // append each argument to the original cmd and params
     {
         if (fork() == 0) // child process
         {
