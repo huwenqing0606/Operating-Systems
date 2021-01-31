d427b79d78b2cb6d40dccde3c17b8611c53aad31
Dr_HuWenqing <huwenqing.pku@gmail.com>
2021-01-30 21:26:20 -0600

    Add files via upload

---
+++ xargs.c
@@ -0,0 +1,48 @@
+#include "kernel/types.h"
+#include "kernel/param.h"
+#include "user/user.h"
+
+#define IN  0
+#define OUT 1
+#define ERR 2
+#define MAXLINE 1024
+
+int main(int argc, char *argv[])
+{
+    char line[MAXLINE];
+    char* params[MAXARG];
+    int n, args_index = 0;
+    int i;
+
+    // in each line, argv[1] is the command cmd
+    // argv[2],...,argv[argc-1] are the paramters
+    char* cmd = argv[1];
+    for (i = 1; i < argc; i++) params[args_index++] = argv[i];
+
+    while ((n = read(IN, line, MAXLINE)) > 0) 
+    // n is the number of bytes read from input and saved to line
+    // read until the end of the IN file
+    {
+        if (fork() == 0) // child process
+        {
+            char *arg = (char*) malloc(sizeof(line));
+            int index = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (line[i] == ' ' || line[i] == '\n') // until a new param or command starts
+                {
+                    arg[index] = 0; // end of arg
+                    params[args_index++] = arg; // save the current arg to params
+                    index = 0; // reset arg index
+                    arg = (char*) malloc(sizeof(line));
+                }
+                else arg[index++] = line[i]; // read each character in line and save to arg
+            }
+            arg[index] = 0;
+            params[args_index] = 0;
+            exec(cmd, params); // execute cmd with params in the current line
+        }
+        else wait((int*)0); // parent process
+    }
+    exit(0);
+}
