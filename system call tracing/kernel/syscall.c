fe0b3ae5722d165633ad535a9d0f7aab6534bee4
Dr_HuWenqing <huwenqing.pku@gmail.com>
2021-02-13 08:47:21 -0600

    Update syscall.c

--- system call tracing/kernel/syscall.c
+++ system call tracing/kernel/syscall.c
@@ -104,7 +104,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_wait(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
-extern uint64 sys_trace(void);
+extern uint64 sys_trace(void);  // add the ``trace" function to retrieve system call arguments from user space
 
 static uint64 (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -128,7 +128,7 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_trace]   sys_trace,    
+[SYS_trace]   sys_trace,    // add the ``trace" function stub
 };
 
 // add names of system calls
