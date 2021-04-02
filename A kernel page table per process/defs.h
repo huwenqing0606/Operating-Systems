cfdad3b5a7d2b0b55c7031d2f815a384c6bb17cb
Dr_HuWenqing <huwenqing.pku@gmail.com>
2021-04-02 16:01:57 -0500

    Add files via upload

--- A kernel page table per process/defs.h
+++ A kernel page table per process/defs.h
@@ -176,7 +176,8 @@ int             uvmcopy(pagetable_t, pagetable_t, uint64);
 void            uvmfree(pagetable_t, uint64);
 void            uvmunmap(pagetable_t, uint64, uint64, int);
 void            uvmclear(pagetable_t, uint64);
-pte_t*          walk(pagetable_t, uint64, int);
+// walk is now used to delete the kernel stack in freeproc() in proc.c, so must be claimed here
+pte_t*          walk(pagetable_t, uint64, int);  
 uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
